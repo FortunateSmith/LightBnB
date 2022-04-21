883cc8fe978cf149be0fa1cd362bdf48cc3b88d1
Liam Smith <liam.s.smith@gmail.com>
2022-04-21 04:04:15 +0000

    add getAllReservations as query function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -79,9 +79,27 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+
+//  const getAllReservations = function (guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
+
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  const queryString = `
+    SELECT properties.*, reservations.*,  AVG(property_reviews.rating) as average_rating
+    FROM reservations
+    JOIN properties ON properties.id = reservations.property_id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE
+      reservations.guest_id = $1 AND
+      reservations.end_date < now()::date
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `;
+  return query.pool(queryString, [guest_id, limit]).then(res => res.row[0]);
+};
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
